e5a2711015f729a01a3f5fbc55e02748875d6be8
maihang123-1 <65518763+maihang123-1@users.noreply.github.com>
2020-06-02 14:55:03 +0700

    Update form 6

--- Form6.cs
+++ Form6.cs
@@ -20,21 +20,33 @@ namespace WindowsFormsApp1
         private void btDoiMk_Click(object sender, EventArgs e)
         {
             SqlConnection connnection = new SqlConnection(@"Data Source=LAPTOP-EFEOHQTE\SQLEXPRESS;Initial Catalog=QuanLyCuaHang1;Integrated Security=True");
+            string sql = "Select TenTaiKhoan From TaiKhoan='" + txtTaiKhoan.Text + "'";
             connnection.Open();
             string sqlSelectDt = "Update TaiKhoan Set TenTaiKhoan='" + txtTaiKhoan.Text + "',MatKhau='" + txtMatkhaumoi.Text + "'";
             if (string.IsNullOrEmpty(txtTaiKhoan.Text) || string.IsNullOrEmpty(txtMatkhaumoi.Text) || string.IsNullOrEmpty(txtXacnhanMk.Text))
                 MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo");
+            else if (txtMatkhaumoi.Text != txtXacnhanMk.Text)
+                MessageBox.Show("Xác nhận lại mật khẩu thất bại, vui lòng nhập lại", "Thông báo");
             else
             {
-                if (txtMatkhaumoi.Text != txtXacnhanMk.Text)
-                    MessageBox.Show("Xác nhận lại mật khẩu thất bại, vui lòng nhập lại", "Thông báo");
+                
+                SqlCommand cmd = new SqlCommand(sql,connnection);
+                SqlDataReader dt = cmd.ExecuteReader();
+                if (dt.Read() == false)
+                {
+                    MessageBox.Show("Tài khoản này không tồn tại", "Thông báo");
+                    
+                }
                 else
                 {
-                    SqlCommand cmd = new SqlCommand(sqlSelectDt, connnection);
-                    cmd.ExecuteNonQuery();
+                    SqlCommand cmd1 = new SqlCommand(sqlSelectDt, connnection);
+                    cmd1.ExecuteNonQuery();
                 }
-
             }
+               
+                
+
+            
         }
 
         private void btThoatF6_Click(object sender, EventArgs e)
