f3f316fdd1814a6526d51739827aaa5bce3f0579
maihang123-1 <65518763+maihang123-1@users.noreply.github.com>
2020-06-20 15:22:40 +0700

    Reset password

--- Form6.cs
+++ Form6.cs
@@ -23,7 +23,19 @@ namespace WindowsFormsApp1
             if (MessageBox.Show("Bạn muốn thoát khỏi ứng dụng ?", "Thông báo", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
                 Application.Exit();
         }
+        private int CheckPass()
+        {
+            int count = 0;
+            for (int i = 0; i < txtMK.TextLength; i++)
+            {
+                count++;
+            }
+            if (count < 6 && count > 0)
+                MessageBox.Show("Mật khẩu tối thiểu 6 ký tự");
+            return count;
 
+
+        }
         private void btReset_Click(object sender, EventArgs e)
         {
             SqlConnection connnection = new SqlConnection(@"Data Source=LAPTOP-EFEOHQTE\SQLEXPRESS;Initial Catalog=QuanLyCuaHang1;Integrated Security=True");
@@ -35,23 +47,28 @@ namespace WindowsFormsApp1
             SqlCommand cmd = new SqlCommand(sqlUpdate, connnection);
             SqlCommand cmd1 = new SqlCommand(sqlSelect, connnection);
             SqlDataReader dr6 = cmd1.ExecuteReader();
-             if ((!dr6.Read()))
+            if ((!dr6.Read()))
             {
                 MessageBox.Show("Tài khoản này không tồn tại", "Thông báo");
             }
             else if (string.IsNullOrEmpty(txtTentk.Text) || string.IsNullOrEmpty(txtMK.Text) || string.IsNullOrEmpty(txtXacNhanMK.Text))
-                MessageBox.Show("Vui lòng nhập đầy đủ thông tin","Thông báo");
-            else if (txtMK.Text != txtXacNhanMK.Text)
-               
-                MessageBox.Show("Vui lòng xác nhận lại mật khẩu","Thông báo");
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin", "Thông báo");
             
-            //else if ((!dr6.Read()))
+               
+            else if (txtMK.Text != txtXacNhanMK.Text)
+
+                MessageBox.Show("Vui lòng xác nhận lại mật khẩu", "Thông báo");
+            //if (!dr6.Read() && CheckPass() >= 6)
             //{
-            //    MessageBox.Show("Tài khoản này không tồn tại", "Thông báo");
-            //}
-            else
+            //    dr6.Close();
+
+
+
+            else if(CheckPass() >= 6)
             {
+                dr6.Close();
                 cmd.Parameters.AddWithValue("MatKhau", txtMK.Text);
+                cmd.ExecuteNonQuery();
                 MessageBox.Show("Reset password thành công");
                 connnection.Close();
             }
